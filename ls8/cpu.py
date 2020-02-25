de1c3be98f460a369ebb9376345a9f785c34dadd
Zach <zpman95@hotmail.com>
2020-02-25 17:49:10 -0600

    completed mult

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,27 +7,24 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 8
+        self.ram = [0] * 256
         self.register = [0] * 8
         self.pc = 0
+        self.rc = 0
         pass
 
-    def load(self):
+    def load(self, program_file):
         """Load a program into memory."""
 
         address = 0
-
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        program = []
+
+        with open(str(program_file)) as f:
+            for line in f:
+                line = line.split(' ')[0].rstrip()
+                program.append(line)
 
         for instruction in program:
             self.ram[address] = instruction
@@ -69,15 +66,22 @@ class CPU:
         """Run the CPU."""
         running = True
         while running:
-            if self.ram[self.pc] == 0b10000010:
+            if self.ram[self.pc] == '10000010':
                 self.register[int(str(self.ram[self.pc + 1]), 2)] = self.ram[self.pc + 2]
                 self.pc += 3
-            elif self.ram[self.pc] == 0b01000111:
+            elif self.ram[self.pc] == '01000111':
                 print(self.register[int(str(self.ram[self.pc + 1]),2)])
                 self.pc += 2
-            elif self.ram[self.pc] == 0b00000001:
+            elif self.ram[self.pc] == '10100010':
+                a = self.register[int(str(self.ram[self.pc + 1]))]
+                b = self.register[int(str(self.ram[self.pc + 2]))]
+                a = int(str(a), 2)
+                b = int(str(b), 2)
+                print(a * b)
+                self.pc += 3
+            elif self.ram[self.pc] == '00000001':
                     self.pc = 0
-                    break
+                    running = False
         def ram_read(self, address):
             return self.ram[int(str(address), 2)]
         def ram_write(self, address, value):
