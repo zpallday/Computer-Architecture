d737220ceb7ce43f1ffc78a68ffee32dd94ea123
Zach <zpman95@hotmail.com>
2020-02-27 15:24:30 -0600

    completed calls

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -24,7 +24,8 @@ class CPU:
         with open(str(program_file)) as f:
             for line in f:
                 line = line.split(' ')[0].rstrip()
-                program.append(line)
+                if len(line) == 8:
+                    program.append(line)
 
         for instruction in program:
             self.ram[address] = instruction
@@ -69,9 +70,11 @@ class CPU:
             if self.ram[self.pc] == '10000010':
                 self.register[int(self.ram[self.pc + 1], 2)] = self.ram[self.pc + 2]
                 self.pc += 3
+
             elif self.ram[self.pc] == '01000111':
                 print(int(self.register[int(self.ram[self.pc + 1],2 )], 2))
                 self.pc += 2
+
             elif self.ram[self.pc] == '10100010':
                 a = self.register[int(self.ram[self.pc + 1], 2)]
                 b = self.register[int(self.ram[self.pc + 2], 2)]
@@ -80,20 +83,32 @@ class CPU:
                 print(a * b)
                 self.pc += 3
 
+            elif self.ram[self.pc] == '10100000':
+                first = self.register[int(self.ram[self.pc +1], 2)]
+                second = self.register[int(self.ram[self.pc + 2], 2)]
+                byte = bin(int(first, 2) + int(second, 2)).lstrip('0b')
+                byte = '0' * (8 - len(byte)) + byte 
+                self.register[int(self.ram[self.pc + 1], 2)] = byte
+                self.pc += 3
+
             elif self.ram[self.pc] == '01000101':
                 self.sp -= 1
                 self.ram[self.sp] = self.register[int(self.ram[self.pc + 1], 2)]
                 self.pc += 2
 
+            elif self.ram[self.pc] == '01010000':
+                self.sp -= 1
+                self.ram[self.sp] = self.pc + 2
+                self.pc = int(self.register[int(self.ram[self.pc + 1], 2)], 2)
+
+            elif self.ram[self.pc] == '00010001':
+                 self.pc = self.ram[self.sp]
+                 self.sp += 1
 
             elif self.ram[self.pc] == '01000110':
                 self.ram[self.sp] = self.register[int(self.ram[self.pc + 1], 2)] = self.ram[self.sp]
                 self.sp += 1
                 self.pc += 2
-
-
-
-
 
             elif self.ram[self.pc] == '00000001':
                     self.pc = 0
